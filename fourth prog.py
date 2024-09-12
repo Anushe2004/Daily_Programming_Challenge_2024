c69cc0be8844fd15e42c3f51914da31ca21e8017
Anushe2004 <134189686+Anushe2004@users.noreply.github.com>
2024-09-12 11:39:18 +0530

    fourth prog.py

--- fourth prog.py
+++ fourth prog.py
@@ -4,7 +4,7 @@ def merge_two_arrays(arr_1, arr_2):
     arr_1 = res_arr[:m]
     arr_2 = res_arr[m:]
     return arr_1, arr_2
-
+#this function works for all the test cases possible i have considered for only one or two test cases
 arr_1 = [1, 3, 5]
 arr_2 = [2, 4, 6]
 print(merge_two_arrays(arr_1, arr_2))
