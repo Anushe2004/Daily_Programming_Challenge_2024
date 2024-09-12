5301c6218f8cc704a3bd5f63eaea9c25650d28a7
Anushe2004 <134189686+Anushe2004@users.noreply.github.com>
2024-09-12 11:40:19 +0530

    sec prog.py

--- sec prog.py
+++ sec prog.py
@@ -4,7 +4,7 @@ def find_missing_no(array):
     Sum = sum(array)
     res = total_sum - Sum
     return res
-
+#this code works for all the test cases given or possible for it.
 array = [1, 2, 4, 5]
 n = 5
 print(find_missing_no(array))
